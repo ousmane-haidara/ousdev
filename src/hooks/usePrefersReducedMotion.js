12e81905ab5ac5514dd332e3a9eaeea9aaff1070
Ousmane HAIDARA <gaetan_haidara@carrefour.com>
2022-04-12 06:52:59 +0200

    add some popular hooks

---
+++ src/hooks/usePrefersReducedMotion.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+const QUERY = '(prefers-reduced-motion: no-preference)';
+const isRenderingOnServer = typeof window === 'undefined';
+
+const getInitialState = () => isRenderingOnServer ? true : !window.matchMedia(QUERY).matches;
+
+function usePrefersReducedMotion() {
+  const [prefersReducedMotion, setPrefersReducedMotion] = useState(getInitialState);
+  useEffect(() => {
+    const mediaQueryList = window.matchMedia(QUERY);
+    const listener = event => {
+      setPrefersReducedMotion(!event.matches);
+    };
+    mediaQueryList.addListener(listener);
+    return () => {
+      mediaQueryList.removeListener(listener);
+    };
+  }, []);
+  return prefersReducedMotion;
+}
+
+export default usePrefersReducedMotion;
+ 
